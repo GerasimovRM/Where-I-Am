64efaad29394a9feeaf61586ce7f4346862d1b0c
Roman M. Gerasimov <romagrizly@gmail.com>
2020-03-26 18:36:17 +0500

    add shotdown resource

--- routes/api_v1.py
+++ routes/api_v1.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 from resources.user import UserResource
 from resources.user_list import UserListResource
+from resources.shutdown import Shutdown
 
 api_v1 = Blueprint('api_v1', __name__)
 
@@ -9,4 +10,5 @@ api = Api(api_v1)
 
 api.add_resource(UserListResource, '/user')
 api.add_resource(UserResource, '/user/<int:user_id>')
+api.add_resource(Shutdown, '/shutdown')
 
